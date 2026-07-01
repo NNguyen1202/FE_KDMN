20780bbe7fe43659a037d15ed7bf1b92be449798
Nguyên Nguyễn <vopvipbmt@gmail.com>
2026-07-01 08:49:01 +0700

    Released v1.2

--- src/pages/UserProfiles.tsx
+++ src/pages/UserProfiles.tsx
@@ -1,25 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+
 import PageBreadcrumb from "../components/common/PageBreadCrumb";
-import UserMetaCard from "../components/UserProfile/UserMetaCard";
-import UserInfoCard from "../components/UserProfile/UserInfoCard";
-import UserAddressCard from "../components/UserProfile/UserAddressCard";
 import PageMeta from "../components/common/PageMeta";
 
+import UserMetaCard from "../components/UserProfile/UserMetaCard";
+import UserInfoCard from "../components/UserProfile/UserInfoCard";
+
+import { getRoleById, getUserById } from "../services/userService";
+
 export default function UserProfiles() {
+  const [user, setUser] = useState<any>(null);
+  const [role, setRole] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  const loadProfile = async () => {
+    try {
+      const currentUser = JSON.parse(localStorage.getItem("user") || "null");
+
+      if (!currentUser?._id) return;
+
+      const res = await getUserById(currentUser._id);
+      const user = res.data.getUser;
+
+      if (!user?.roleID) return;
+
+      // Nếu roleID là ObjectId
+      const roleId =
+        typeof user.roleID === "string" ? user.roleID : user.roleID._id;
+
+      const roleRes = await getRoleById(roleId);
+
+      console.log("Lấy role người dùng: ", roleRes);
+
+      setUser(user);
+      setRole(roleRes.data);
+
+      console.log("User hiện tại: ",user);
+      
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
+  if (loading) {
+    return <div className="p-6">Loading...</div>;
+  }
+
   return (
     <>
-      <PageMeta
-        title="React.js Profile Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Profile Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
-      />
+      <PageMeta title="User Profile" description="User Profile" />
+
       <PageBreadcrumb pageTitle="Profile" />
+
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
-        <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
-          Profile
-        </h3>
+        <h3 className="mb-5 text-lg font-semibold">Hồ sơ cá nhân</h3>
+
         <div className="space-y-6">
-          <UserMetaCard />
-          <UserInfoCard />
-          <UserAddressCard />
+          <UserMetaCard user={user} role={role} />
+
+          <UserInfoCard user={user} role={role} reload={loadProfile} />
+
+          {/* <UserAddressCard user={user} /> */}
         </div>
       </div>
     </>
